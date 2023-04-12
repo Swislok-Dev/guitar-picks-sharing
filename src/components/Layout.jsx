a36d73094c5f60068eb529cd650971a66a390cff
cm@MBA <cmarker4@gmail.com>
2023-04-12 09:30:21 -0500

    style Layout and animate add button

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,16 +2,16 @@ import Head from 'next/head';
 import Link from 'next/link';
 import React from 'react';
 
-function Layout() {
+function Layout({ children }) {
   return (
     <>
       <Head>
         <title>Guitar Picks</title>
         <meta name="description" content="photo sharing site" />
       </Head>
-      <header className="top-0 flex justify-between p-8 items-center sticky bg-gray-400">
-        <Link href="#">
-          <h1 className="text-3xl">Guitar Picks</h1>
+      <header className="z-10 top-0 flex justify-between p-8 items-center sticky bg-gray-400">
+        <Link href="/">
+          <h1>Guitar Picks</h1>
         </Link>
         <nav className="">
           <Link href="#">
@@ -21,6 +21,8 @@ function Layout() {
           </Link>
         </nav>
       </header>
+
+      <main>{children}</main>
     </>
   );
 }
