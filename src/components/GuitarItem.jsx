aa3b1c78cdb0b2ba514f3a96c07be310991838e1
cm@MBA <cmarker4@gmail.com>
2023-04-12 14:04:11 -0500

    add Next image loader

--- src/components/GuitarItem.jsx
+++ src/components/GuitarItem.jsx
@@ -1,10 +1,21 @@
+import Image from 'next/image';
 import React from 'react';
 
 function GuitarItem({ imageUrl }) {
-  
+  const myLoader = () => {
+    return `${imageUrl}?w=500&q=75`;
+  };
+
   return (
-    <div className="border-gray-500 border-2 p-4  m-2">
-      <img className="object-scale-down cursor-pointer" src={imageUrl} alt="guitar photo" />
+    <div className="guitar-item rounded p-4  m-2">
+      <Image
+        loader={myLoader}
+        width={500}
+        height={800}
+        className=" object-scale-down cursor-pointer"
+        src={imageUrl}
+        alt="guitar photo"
+      />
     </div>
   );
 }
