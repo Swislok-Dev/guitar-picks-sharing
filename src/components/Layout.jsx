9a57a60adf9f3383d2019e5790ff636842ca9b09
cm@MBA <cmarker4@gmail.com>
2023-04-14 09:17:59 -0500

    chore: background image covers 100%; text color

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -9,17 +9,17 @@ function Layout({ children }) {
         <title>Guitar Picks</title>
         <meta name="description" content="photo sharing site" />
       </Head>
+      <div className="bg"></div>
       <header className="z-10 top-0 flex justify-between p-6 items-center sticky bg-gray-400">
         <Link href="/">
           <h1>Guitar Picks</h1>
         </Link>
         <nav className="">
           <Link href="#">
-              <h2>Account</h2>
+            <h2>Account</h2>
           </Link>
         </nav>
       </header>
-      <div className="bg"></div>
       <main>{children}</main>
     </>
   );
