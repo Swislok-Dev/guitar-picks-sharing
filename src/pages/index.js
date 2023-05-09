705f840964c81c3615240ae9f154b2711dedd892
cm@MBA <cmarker4@gmail.com>
2023-05-09 08:31:14 -0500

    pull server side data for guitarss

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,19 +3,14 @@ import GuitarItem from '../components/GuitarItem';
 import Layout from '../components/Layout';
 import Modal from '../components/Modal';
 import AddGuitarForm from '@/components/guitar/AddGuitarForm';
+import clientPromise from '../../lib/mongodb';
 
-export default function Home() {
+export default function Home({ guitars }) {
   const [show, setShow] = React.useState(false);
 
   const onClose = () => {
-    setShow(false)
-  }
-  const exampleImage1 =
-    'https://m.media-amazon.com/images/I/71ZlrIEmxOL._AC_SY879_.jpg';
-  const exampleImage2 =
-    'https://c1.zzounds.com/media/productmedia/fit,600by800/quality,85/1_Full_Straight_Front_40878-415561e4d53a9ab83db782d02d3487bf.jpg';
-  const exampleImage3 =
-    'https://m.media-amazon.com/images/I/51dk8encHuL._AC_SL1200_.jpg';
+    setShow(false);
+  };
 
   return (
     <Layout>
@@ -26,16 +21,27 @@ export default function Home() {
         </div>
       </div>
       <div className="my-20 flex flex-wrap justify-around gap-4">
-        <GuitarItem imageUrl={exampleImage1} />
-        <GuitarItem imageUrl={exampleImage2} />
-        <GuitarItem imageUrl={exampleImage3} />
+        {guitars.map((guitar) => (
+          <GuitarItem guitar={guitar} key={guitar.id} />
+        ))}
       </div>
       <Modal
         title="Add a guitar"
         show={show}
         onClose={() => onClose()}
-        content={<AddGuitarForm onClose={onClose}/>}
+        content={<AddGuitarForm onClose={onClose} />}
       />
     </Layout>
   );
+}
+
+export async function getServerSideProps() {
+  const client = await clientPromise;
+  const db = client.db('guitar-picks');
+  const guitars = await db.collection('guitars').find({}).toArray();
+  return {
+    props: {
+      guitars: JSON.parse(JSON.stringify(guitars)),
+    },
+  };
 }
