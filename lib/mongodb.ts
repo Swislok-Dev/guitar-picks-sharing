2aaceaf2392a5bbfc4c5129d16513f54f02bf150
cm@MBA <cmarker4@gmail.com>
2023-05-09 08:45:52 -0500

    fix double equals error

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -1,20 +1,27 @@
-import { MongoClient } from "mongodb";
+import { MongoClient } from 'mongodb';
 
 if (!process.env.MONGODB_URI) {
-  throw new Error('Invalid/Missing environment variable: "MONGODB_URI"');
+  throw new Error(
+    'Invalid/Missing environment variable: "MONGODB_URI"'
+  );
 }
-const uri: string = '';
-if (process.env.NODE_ENV === "development") {
-  uri == process.env.LOCAL_MONGODB_URI;
-} else {
-  uri == process.env.MONGODB_URI
-}
+// const uri: string = '';
+// if (process.env.NODE_ENV === "development") {
+//   uri == process.env.LOCAL_MONGODB_URI;
+// } else {
+//   uri = process.env.MONGODB_URI
+// }
+const uri =
+  process.env.NODE_ENV === 'production'
+    ? process.env.MONGODB_URI
+    : process.env.LOCAL_MONGODB_URI;
+
 const options = {};
 
 let client;
 let clientPromise: Promise<MongoClient>;
 
-if (process.env.NODE_ENV === "development") {
+if (process.env.NODE_ENV === 'development') {
   // In development mode, use a global variable so that the value
   // is preserved across module reloads caused by HMR (Hot Module Replacement).
   if (!global._mongoClientPromise) {
